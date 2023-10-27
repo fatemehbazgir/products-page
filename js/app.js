4445dd0f5fb2daa51e6e2cf009737c5d2ad69146
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-10-27 17:11:19 +0330

    Event definition for category buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
 const searchHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
   products.forEach((product) => {
@@ -11,5 +12,20 @@ const searchHandler = (event) => {
     }
   });
 };
-
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
+  products.forEach((product) => {
+    const category = product.dataset.category;
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
 searchInput.addEventListener("keyup", searchHandler);
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
